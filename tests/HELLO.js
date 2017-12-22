822517bcd939b85ff43f1c0e219704b73b20f27f
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-22 14:08:06 +0100

    Updated tests

--- tests/HELLO.js
+++ tests/HELLO.js
@@ -11,15 +11,17 @@ const childProcess = require("child_process");
 /* First it executes it, then it shows */
 /* the plain code.                     */
 
-function runScript(scriptPath, plain, callback) {
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+function runScript(scriptPath, plain, callback){
     let invoked = false;
     let process = childProcess.fork(scriptPath, (plain ? ["../examples/HELLO.lol", "--plain"] : ["../examples/HELLO.lol"]));
-    process.on("error", function (err) {
+    process.on("error", function(err) {
         if (invoked) return;
         invoked = true;
         callback(err);
     });
-    process.on("exit", function (code){
+    process.on("exit", function(code){
         if (invoked) return;
         invoked = true;
         var err = code === 0 ? null : new Error("Exit code: " + code);
@@ -28,4 +30,5 @@ function runScript(scriptPath, plain, callback) {
 }
 
 runScript("../app", false, function (err){ if (err) throw err; });
+sleep(500);
 runScript("../app", true,  function (err){ if (err) throw err; });
