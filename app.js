caa191b91c729f760d11809a80889d58fe30a900
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-22 13:20:43 +0100

    Changed default behaviour

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ let parse = require("./parser");
 
 const isset = function(obj){ return !!(obj && obj !== null && (typeof obj === "string" && obj !== "")); };
 
-const parser = function(code, plain){
+const parser = function(code, notPlain){
     parse(code, function(err, warn, fin){
         if (err) return console.log(err);
         if (warn) console.log(warn)
-        plain ? console.log(fin) : eval(fin);
+        notPlain ? eval(fin) : console.log(fin);
         return true;
     });
 };
 
 const spawnShell = function(){
     let file = process.argv.slice(2)[0];
-    let doPlain = false;
+    let noPlain = true;
     let plain = "";
     if (!isset(file)) return console.log("Error: No input file.");
     fs.readFile(file, "utf8", function(err, data){
         if (err) return console.log("Error: " + err);
         plain = (isset(process.argv.slice(3)[0]) ? process.argv.slice(3)[0] : "");
-        if (plain.toLowerCase() == "--plain" || plain.toLowerCase() == "-p") doPlain = true;
-        parser(data, doPlain);
+        if (plain.toLowerCase() == "--plain" || plain.toLowerCase() == "-p") noPlain = false;
+        parser(data, noPlain);
     });
 };
 
